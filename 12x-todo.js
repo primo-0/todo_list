c7787ba27fc9dd2f4baf80a30cad28a5412fdab7
Justin Augustus Macaraeg <justinaugustusm@gmail.com>
2026-06-29 08:55:48 +0800

    initial commit

---
+++ 12x-todo.js
@@ -0,0 +1,68 @@
+//eto yung object na pinag setan ng storage kaya dito ren ipaparse......
+const todoList = JSON.parse(localStorage.getItem('todoList')) ||
+    [{
+      name: 'make dinner',
+      dueDate: '2022-12-22'
+    },{
+      name: 'wash dishes',
+      dueDate: '2022-12-22'
+    }];
+   
+renderTodoList();
+
+function renderTodoList() {
+
+let todoListHTML = '';
+
+
+todoList.forEach(function(todoObject, index) {
+  
+   const { name, dueDate } = todoObject;
+   const html = `
+    <div>${name}</div>
+    <div>${dueDate}</div>   
+    <button onclick= "
+    todoList.splice(${index}, 1);
+    renderTodoList(); 
+    saveToStorage(); 
+    " class = "delete-todo-button">Delete</button>
+    
+    `;
+   todoListHTML += html;
+});
+for (let i = 0; i < todoList.length; i++) {
+   
+}
+
+
+
+document.querySelector ('.js-todo-list').innerHTML = `${todoListHTML}`;
+}
+
+
+
+function addTodo(){
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+  todoList.push
+  ({
+      //name:name,
+      //dueDate: dueDate
+      name,
+      dueDate
+  });
+  
+
+  inputElement.value = '';
+  renderTodoList();
+  //this is to save storage..... 
+  saveToStorage();
+  
+}
+//this function is to set the storage and iseset sa const or object
+function saveToStorage (){
+  localStorage.setItem('todoList', JSON.stringify(todoList));
+}
